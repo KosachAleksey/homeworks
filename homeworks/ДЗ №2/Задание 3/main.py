c127fddec51ec2f2cdabb76ae338404e6a35de55
Kosach <kosach.aleksey@vk.com>
2025-02-06 10:24:48 +0300

    Мои ДЗ

---
+++ homeworks/ДЗ №2/Задание 3/main.py
@@ -0,0 +1,10 @@
+meters = int(input("Enter the number of meters:"))
+centimeters = meters * 100
+decimeters = meters * 10
+millimeters = meters * 1000
+miles = meters / 1609.34  # миля = 1609.34 метра
+print(meters, "meters is equal to:")
+print(centimeters, "centimeters")
+print(decimeters, "decimeters")
+print(millimeters, "millimeters")
+print(miles, "miles")
